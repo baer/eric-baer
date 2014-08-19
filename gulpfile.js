c0c92cd8d83692e45f17682c36c0407c8691adb6
Eric Baer <me@ericbaer.com>
2014-08-19 10:51:50 -0700

    Add jshint

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp   = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', ['lint']);
